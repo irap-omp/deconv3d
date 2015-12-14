Fix the convergence problems. Arbitrarily set the gibbs variance to 5.

lib/line_models.py
@@ -1,6 +1,4 @@
-from numpy.core.numeric import Infinity
 import numpy as np
-from rtnorm import rtnorm
 
 
 class LineModel:
@@ -63,7 +61,7 @@ class LineModel:
 class SingleGaussianLineModel(LineModel):
     """
     A single gaussian curve, defined by its three usual parameters.
-    This is the default line model that deconv3d uses.
+    This is the default line model that `deconv3d` uses.
     """
 
     def parameters(self):
@@ -77,8 +75,8 @@ class SingleGaussianLineModel(LineModel):
 
     def max_boundaries(self, runner):
         """
-        The FSF is normalized, so we need to adjust the maximum of our amplitude
-        accordingly.
+        The FSF is normalized, so we need to adjust the maximum of our
+        amplitude accordingly.
         """
         cube = runner.cube
         fsf = runner.fsf
@@ -103,6 +101,6 @@ class SingleGaussianLineModel(LineModel):
         c: Center
         w: Standard deviation, aka. RMS Width
 
-        If `x` is an `ndarray`, the return value will be an `ndarray` too.
+        If `x` is a `ndarray`, the return value will be a `ndarray` too.
         """
         return a * np.exp(-1. * (x - c) ** 2 / (2. * w ** 2))

lib/math_utils.py
@@ -1,14 +1,10 @@
 import numpy as np
-import math
-
-# Mandatory TAU = 6.283185307179586...
-math.tau = 2 * math.pi
 
 
 def merge_where_nan(target, filler):
     """
-    Procedurally mutates `target` by replacing its nan values by values from
-    `filler`. This is a very simple polyfill for `numpy.copyto`. (for numpy<1.7)
+    Mutates `target` by replacing its nan values by values from `filler`.
+    This is a very simple polyfill for `numpy.copyto`. (for numpy<1.7)
     """
     try:
         np.copyto(target, filler, where=np.isnan(target))
@@ -20,8 +16,6 @@ def merge_where_nan(target, filler):
 def median_clip(data, clip_sigma=3., limit_ratio=1e-3, max_iterations=5):
     """
     Computes an iteratively sigma-clipped median on a `data` set.
-
-    /!\ It mutates the input `data`.
 
     data : ndarray
         Input data.

lib/spread_functions.py
@@ -232,10 +232,15 @@ class GaussianLineSpreadFunction(LineSpreadFunction):
         # Resulting vector length is the spectral depth of the cube
         depth = for_cube.shape[0]
         # Assymmetric range around 0
-        zo = (depth - 1) / 2 - (depth % 2 - 1)
-        z_range = np.arange(depth) - zo
+        z_center_index = (depth - 1) / 2 - (depth % 2 - 1)
+        z_range = np.arange(depth) - z_center_index
         # Compute gaussian (we assume peak is at 0, ie. µ=0)
-        lsf_1d = self.gaussian(z_range, 0, sigma)
+        if sigma == 0:
+            lsf_1d = np.zeros(depth)
+            lsf_1d[z_center_index] = 1.
+        else:
+            # fixme µ is 0, check if that's is a problem
+            lsf_1d = self.gaussian(z_range, 0, sigma)
         # Normalize and serve
         return lsf_1d / lsf_1d.sum()
 